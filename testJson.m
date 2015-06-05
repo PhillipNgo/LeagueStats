5e71a2e4ce0fa7cac43ad33ffbe3b7f5516e211b
Cthulhuehue <cmstanley21@gmail.com>
2015-06-05 16:23:40 -0400

    lel

--- testJson.m
+++ testJson.m
@@ -2,3 +2,4 @@ link = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json';
 link2 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion/Aatrox.json';
 data = parse_json(urlread(link))
 data2 = parse_json(urlread(link2))
+%imhot
