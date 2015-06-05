0cb9658bc843864d3eaaeb97e70fceb3e6f2c443
Phillip Ngo <phllpqng@gmail.com>
2015-06-05 16:16:44 -0400

    added to testJSON

--- testJson.m
+++ testJson.m
@@ -1,2 +1,4 @@
 link = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json';
+link2 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion/Aatrox.json';
 data = parse_json(urlread(link))
+data2 = parse_json(urlread(link2))
